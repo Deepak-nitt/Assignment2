e880773129fe81620302df31fb0e1e47b9d2f518
Deepak-nitt <deepak.kumar@cleaff.com>
2025-08-20 23:02:18 +0530

    Header added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header],
+  imports: [CommonModule,Header],  
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  isformVisible = signal(false);
 }
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Output , EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  @Output() addClicked = new EventEmitter<void>();
+
+  onAddClick(){
+    this.addClicked.emit();
+  }
 
 }
