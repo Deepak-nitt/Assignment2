cfb1175a36c27ed27765a22b0211f83e35133aa0
Deepak-nitt <deepak.kumar@cleaff.com>
2025-08-21 17:31:21 +0530

    form created, validation added, file structure changed

---
+++ src/app/features/portfolio/models/portfolio.model.ts
@@ -0,0 +1,8 @@
+// creating a model for the portfolio form value
+
+export interface PortfolioFormValue {
+  name: string;
+  skillsCsv: string;
+  projectTitle: string;
+  projectDescription: string;
+}
