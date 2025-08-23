cacd11496b57b5014139ea8e1f7bdd93798e0e65
Deepak-nitt <deepak.kumar@cleaff.com>
2025-08-23 20:46:56 +0530

    Search based on the condition

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,25 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PortfolioFormValue } from '../portfolio/models/portfolio.model';
+import { SearchBoxComponent } from '../../shared/search-box/search-box';
+import { filterAndSortPortfolios } from '../../core/utils/ordering-seaching.utils';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SearchBoxComponent],
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.css']
 })
 export class Dashboard {
   @Input() portfolios: PortfolioFormValue[] = [];
-  // getter for sorted portfolios by createdAt descending
-  get sortedPortfolios(): PortfolioFormValue[] {
-    return [...this.portfolios].sort((a, b) => {
-      const nameA = a.name.trim().toLowerCase();
-      const nameB = b.name.trim().toLowerCase();
-      if (nameA < nameB) return -1;
-      if (nameA > nameB) return 1;
-      // If names are equal, sort by createdAt DESC (newest first)
-      return b.createdAt - a.createdAt;
-    });
+  searchTerm = '';
+
+  onSearchChange(value: string) {
+    this.searchTerm = value;
+  }
+
+  get filteredPortfolios(): PortfolioFormValue[] {
+    return filterAndSortPortfolios(this.portfolios, this.searchTerm);
   }
 }
